49a3c7d2d9753f99a50e7fefa2ce94bebbbe9124
Chelsea Smith <cms11231@gmail.com>
2020-09-09 23:59:11 -0600

    Value of Quadratic

---
+++ Chapter9/ValofQuad.java
@@ -0,0 +1,9 @@
+public class ValofQuad
+{  
+  public static void main ( String[] args )  
+  {
+    double X = 2 / 3;
+    double quadratic = 3 * X * X - 8 * X + 4;
+    System.out.println("At X = " + X + " the value of the quadratic is " + quadratic);
+  }
+}
