45f8722d7e8afa14d5e7aedc27877745f9a7d870
Chelsea Smith <cms11231@gmail.com>
2020-09-10 00:19:35 -0600

    Average Rainfall :)

---
+++ Chapter10/AverageRainfall.java
@@ -0,0 +1,14 @@
+public class AverageRainfall
+{  
+    public static void main ( String[] args )  
+    {
+        double april = 12;
+        double may = 14;
+        double june = 8;
+        double average = (12 + 14 + 8) / 3;
+        System.out.println ("Rainfall for April: \t" + april);
+        System.out.println ("Rainfall for May: \t" + may);
+        System.out.println ("Rainfall for June: \t" + june);
+        System.out.println ("Average Rainfall: \t" + average);
+    }
+}
