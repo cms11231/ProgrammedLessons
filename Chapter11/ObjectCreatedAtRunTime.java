e50bb517083363bbbd89286d1b095cae92a0bd38
Chelsea Smith <cms11231@gmail.com>
2020-09-14 13:51:20 -0600

    Objecct Created At Runtime :)

---
+++ Chapter11/ObjectCreatedAtRunTime.java
@@ -0,0 +1,12 @@
+public class ObjectCreatedAtRunTime
+{  
+    public static void main ( String[] args )  
+    {
+        String str ;
+        str = new String("Hello World");
+        System.out.println( str );
+        String secondvariable ;
+        secondvariable = new String("Hellooooo World");
+        System.out.println( secondvariable );
+    }
+}
