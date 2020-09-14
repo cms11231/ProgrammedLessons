6a015e0825b90f727d33cdc0e627984beb599b58
Chelsea Smith <cms11231@gmail.com>
2020-09-14 14:08:30 -0600

    String Method that creates new String :)

--- Chapter11/StringMeth.java
+++ Chapter11/StringMeth.java
@@ -1,10 +1,12 @@
-public class StringLength
+public class StringMeth
 {
     public static void main ( String[] args )
     {
-        String first = new String("    In a Hole    in the ground there lived a Hobbit.   ");
+        String first = new String("                 In a Hole in the ground there lived a Hobbit.   ");
         String second;
 
         second = first.trim();
+        System.out.println(first);
+        System.out.println(second);
     }
 }
