7e5541a2f96ff8de51df3bb1ee9a9d5a94d3a721
Chelsea Smith <cms11231@gmail.com>
2020-09-14 13:57:34 -0600

    String Length :)

---
+++ Chapter11/StringLength.java
@@ -0,0 +1,14 @@
+public class StringLength
+{
+    public static void main ( String[] args )
+    {
+        String str;
+        int    len;
+        
+        str = new String( "hello" );
+
+        len = str.length();
+
+        System.out.println("The length is: " + len );
+    }
+}
