ad7b1731077ad33f235fa37bdd1d68705669b4b2
Chelsea Smith <cms11231@gmail.com>
2020-09-17 11:10:00 -0600

    Order Checker :)

---
+++ Chapter16/OrderChecker.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+public class OrderChecker
+{
+	public static void main(String[] args) {
+
+        double bolts;
+        double nuts;
+        double washer;
+        
+        Scanner scan = new Scanner(System.in);
+	    System.out.println("Enter amount of bolts: ");
+        bolts = scan.nextDouble();
+        System.out.println("Enter amount of nuts: ");
+        nuts = scan.nextDouble();
+        System.out.println("Enter amount of washers: ");
+        washer = scan.nextDouble();
+        
+        boolean enoughnuts = false;
+        
+        if (bolts <= nuts) {
+            enoughnuts = true;
+        }
+
+        if (enoughnuts) {
+            System.out.println("\nCheck the order: Enough  nuts");
+        } else {
+            System.out.println("\nCheck the order: Not enough nuts");
+        }
+
+        double cost = bolts * 5 + nuts * 3 + washer;
+        
+        System.out.println("\nTotal cost: " + cost);
+
+    }
+}
+
+// The distance a brick travels in 100 seconds is 160,870.0 feet.
