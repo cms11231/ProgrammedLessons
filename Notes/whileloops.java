19149525b75b0f210d197870455c1288c1e52c19
Chelsea Smith <cms11231@gmail.com>
2020-09-24 23:57:10 -0600

    Notes

---
+++ Notes/whileloops.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class whileloops {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        // int count = 0;
+        // while (count<10) {
+        //     System.out.println(count);
+
+        //     count = count + 1;
+        // }
+        
+        
+        // Scanner scan = new Scanner(System.in);
+
+        // System.out.println("Enter the password: ");
+        // String password = scan.next();
+
+        // while (!password.equals("password")) {
+        //     System.out.println("Incorrect, try again: ");
+        //     password = scan.next();
+        // }
+
+        // scan.close();
+
+
+
+        // int x = 1;
+        // while (x > 0) {
+        //     x = 0;
+        //     System.out.println("In loop");
+        //     x = 1;
+        // }
+
+
+
+        // while (true) {
+        //     forever
+        //     use ctrl+c to stop from running
+        // }
+
+
+
+        // int z = 0;
+        // while (z < 100) {
+        //     System.out.println("Oops, z does not change");
+        // }
+
+
+
+        // int foo = 0;
+        // while (foo < 10) {
+        //     int bar = 1;
+        //     foo++;
+        // }
+    }
+}
