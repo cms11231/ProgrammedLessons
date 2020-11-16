2bd517eef7922e3d8cac8fb2b8c4ca2348390538
Chelsea Smith <cms11231@gmail.com>
2020-11-15 23:54:24 -0700

    Finish

--- Challenges/LunchAccount.java
+++ Challenges/LunchAccount.java
@@ -1,21 +1,54 @@
 public class LunchAccount{
-    private String name;
-    private Integer balance;
-    private Integer price;
+    private double balance;
+    private int price;
+    private int id;
+    public int totalMoneySpent = 0;
+    private static int idcount = 0;
+    private static int lunchesSold = 0;
+    private static boolean first100 = false;
 
 
-    public LunchAccount (str name, int balance) {
-        this.name = name;
-        this.balance = balance;
+
+
+    public LunchAccount() {
+        this.id = idcount + 1000;
+        this.balance += balance;
+        this.idcount++;
+        if (id<1100) {
+            this.balance += 20;
+            first100 = true;
+        }
+    }
+    
+    public LunchAccount( double balance ) {
+        this.id = idcount + 1000;
+        this.idcount++;
+        if (id<1100) {
+            this.balance += 20;
+            first100 = true;
+        }
+    }
+    
+    public addBalance( double add) {
+        this.balance += add;
     }
 
+    public int getBalance() {
+        return this.balance;
+    }
 
+    public int getId() {
+        return this.id;
+    }
 
-
-    public void buylunch(int price){
-        this.price += price;
-        this.totalpaid += price;
-        
+    public buyFood ( int price ) {
+        if ( balance >= price) {
+            balance -= price;
+            lunchesSold ++;
+            totalMoneySpent += price;
+        } else {
+            return "Error: Not enough funds";
+        }
     }
 
 
--- Challenges/LunchAccountTester.java
+++ Challenges/LunchAccountTester.java
@@ -1,9 +1,21 @@
 public class LunchAccountTester {
     public static void main(String[] args) {
-        LunchAccount Mary = new LunchAccound("Mary Sue", )
+        LunchAccount Jackson = new LunchAccount(10);
+        LunchAccount Joe = new LunchAccount();
+
+		System.out.println(Jackson.getId());
+	    System.out.println(Joe.getId());
+
+		System.out.println(Jackson.getBalance());
+	    System.out.println(Joe.getBalance());
+    
+        System.out.println(Jackson.getId());
+	    System.out.println(Joe.getId());
+    
+    
+    
+    
+    
     }
-
-
-
 
 }
